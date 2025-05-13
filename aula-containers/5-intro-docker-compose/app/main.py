0b4b8cb93b01f0ff75ab8de31aaba562c018d335
Frankllin Teixeira <frankllin15@hotmail.com>
2025-05-12 23:18:34 -0300

    changes

--- aula-containers/5-intro-docker-compose/app/main.py
+++ aula-containers/5-intro-docker-compose/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import os
 
-USERNAME = os.environ.get('USERNAME')
+USUARIO = os.environ.get('USUARIO')
 
 app = FastAPI()
 
@@ -11,4 +11,4 @@ async def root():
 
 @app.get("/variavel_ambiente")
 async def verificar_variavel_de_ambiente():
-    return {"USERNAME": USERNAME}
+    return {"USUARIO": USUARIO}
